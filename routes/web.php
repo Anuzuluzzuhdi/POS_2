9b5b342201a578c9ce4ccbd72d78d788f3779d10
Anuzuluzzuhdi <abhinaya.zuhdi@gmail.com>
2025-04-18 23:37:48 +0700

    Prak 2 -Penerapan layouting

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserController2;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\WelcomeController;
 
 Route::get('/', [HomeController::class, 'index']);
 
@@ -24,6 +25,7 @@ Route::get('/penjualan', [PenjualanController::class, 'index']);
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/user/tambah', [UserController::class, 'tambah']);
 Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
