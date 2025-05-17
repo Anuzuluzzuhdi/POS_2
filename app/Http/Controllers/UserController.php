42d6d3442c8ccdd46f7929e66e7cc7002737a0d2
Anuzuluzzuhdi <abhinaya.zuhdi@gmail.com>
2025-05-18 01:06:26 +0700

    filtering dan searching

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,28 +19,35 @@ class UserController extends Controller
         ];
 
         $activeMenu = 'user'; // set menu yang sedang aktif
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        //return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+
+        $level = LevelModel::all();
+        return view('user.index', [
+            'breadcrumb' => $breadcrumb, 
+            'page' => $page, 
+            'activeMenu' => $activeMenu,
+            'level' => $level]);
     }
 
-    public function list(Request $request)
-    {
-        $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
-        ->with('level');
-
-        return DataTables::of($users)
-        // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
-        ->addIndexColumn()
-        ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
-        $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn- sm">Detail</a> ';
-        $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
-        $btn .= '<form class="d-inline-block" method="POST" action="'. url('/user/'.$user->user_id).'">'
-        . csrf_field() . method_field('DELETE') .
-        '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
-        return $btn;
-            })
-                ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
-                ->make(true);
-            }
+    public function list(Request $request){
+        $users = UserModel::select('user_id','username','nama','level_id')->with('level');
+        
+        if($request->level_id){
+            $users->where('level_id',$request->level_id);
+        }
+        return DataTables::of($users) 
+        ->addIndexColumn()  
+        ->addColumn('aksi', function ($user) {  // menambahkan kolom aksi 
+            $btn  = '<a href="'.url('/user/' . $user->user_id. '/detail').'" class="btn btn-info btn-sm">Detail</a> '; 
+            $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> '; 
+            $btn .= '<form class="d-inline-block" method="POST" action="'. url('/user/'.$user->user_id).'">' 
+                    . csrf_field() . method_field('DELETE') .  
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';                  
+            return $btn; 
+        }) 
+        ->rawColumns(['aksi']) 
+        ->make(true); 
+}
 
     public function create()
     {
