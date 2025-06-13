29bc7163ba56009e9d5c63d648876761c0209e49
Anuzuluzzuhdi <abhinaya.zuhdi@gmail.com>
2025-06-13 22:36:24 +0700

    Prak 3 - delete ajax

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,9 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}/edit', [UserController::class, 'edit']);       // menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']);          // menyimpan perubahan data user
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);       // menampilkan halaman form edit user Ajax
-    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);          // menyimpan perubahan data user Ajax
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // menyimpan perubahan data user Ajax
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);       // form delete user Ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);    //UNtuk hapus data user Ajax
     Route::delete('/{id}', [UserController::class, 'destroy']);       // menghapus data user
 });
 
